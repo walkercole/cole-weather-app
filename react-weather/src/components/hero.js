5160924df965a6748908247b64809a7ded73037d
Walker Cole <besidethebrave@gmail.com>
2023-05-10 16:12:21 -0700

    alt text logo

--- react-weather/src/components/hero.js
+++ react-weather/src/components/hero.js
@@ -2,7 +2,7 @@ function Hero () {
     
  return (
     <section className="relative isolate overflow-hidden px-6 mt-8 lg:px-8 bg-gradient">
-        <img className="mx-auto h-60" src="img/crosswindLogoFlat.png" alt="" />
+        <img className="mx-auto h-60" src="img/crosswindLogoFlat.png" alt="crosswind" />
     </section>
  )
 }
