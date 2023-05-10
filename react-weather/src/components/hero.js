12b5b0365f2e74ff950754c335da88070bcece21
Walker Cole <besidethebrave@gmail.com>
2023-05-10 16:16:06 -0700

    update route for img

--- react-weather/src/components/hero.js
+++ react-weather/src/components/hero.js
@@ -2,7 +2,7 @@ function Hero () {
     
  return (
     <section className="relative isolate overflow-hidden px-6 mt-8 lg:px-8 bg-gradient">
-        <img className="mx-auto h-60" src="img/crosswindLogoFlat.png" alt="crosswind" />
+        <img className="mx-auto h-60" src="img/crossWindHero.png" alt="crosswind" />
     </section>
  )
 }
